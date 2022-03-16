53c060e69ec4a90c7408efb382026e39cdd8fff6
Tay Starr <tstarrroberts@gmail.com>
2022-03-16 05:33:57 -0400

    rough layout first question

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,52 @@ function countdown() {
         timeLeft--;
         if (timeLeft === -1) {
             clearInterval(timeInterval);
+            // don't forget this console.log
             console.log("Show times up message here")
         }
     }, 1000);
 }
 
+var startQuiz = function() {
+    var page =document.getElementById('main-page');
+
+    if (page.style.display === 'none') {
+        page.style.display = 'block';
+    } else {
+        page.style.display = 'none';
+    }
+
+    firstQues();
+};
+
+var firstQues = function() {
+    var firstAsk = document.getElementById('first');
+    firstAsk.className = "header";
+    firstAsk.textContent = "This is the first question!";
+
+    var firstAnsw = document.createElement("button");
+    firstAnsw.className = "button";
+    firstAnsw.innerHTML = "<ol class='button'>One</ol>";
+    firstAsk.appendChild(firstAnsw); 
+
+    var secAnsw = document.createElement("button");
+    secAnsw.className = "button";
+    secAnsw.innerHTML = "<ol class='button'>One</ol>";
+    firstAsk.appendChild(secAnsw); 
+
+    var thirdAnsw = document.createElement("button");
+    thirdAnsw.className = "button";
+    thirdAnsw.innerHTML = "<ol class='button'>One</ol>";
+    firstAsk.appendChild(thirdAnsw); 
+
+    var fourthAnsw = document.createElement("button");
+    fourthAnsw.className = "button";
+    fourthAnsw.innerHTML = "<ol class='button'>One</ol>";
+    firstAsk.appendChild(fourthAnsw); 
+
+}
+
 var startBtn = document.querySelector("#start");
 
-console.log(countdown);
-
 startBtn.addEventListener("click", countdown);
+startBtn.addEventListener("click", startQuiz);
